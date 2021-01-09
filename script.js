54a81442106cb55362ae501943be0e0ece596783
SantiagoLuna1920 <kobyj103@gmail.com>
2021-01-09 01:12:52 -0500

    Agregado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const trivia = {"trivia_categories":[{"id":9,"name":"General Knowledge"},{"id":10,"name":"Entertainment: Books"},{"id":11,"name":"Entertainment: Film"},{"id":12,"name":"Entertainment: Music"},{"id":13,"name":"Entertainment: Musicals & Theatres"},{"id":14,"name":"Entertainment: Television"},{"id":15,"name":"Entertainment: Video Games"},{"id":16,"name":"Entertainment: Board Games"},{"id":17,"name":"Science & Nature"},{"id":18,"name":"Science: Computers"},{"id":19,"name":"Science: Mathematics"},{"id":20,"name":"Mythology"},{"id":21,"name":"Sports"},{"id":22,"name":"Geography"},{"id":23,"name":"History"},{"id":24,"name":"Politics"},{"id":25,"name":"Art"},{"id":26,"name":"Celebrities"},{"id":27,"name":"Animals"},{"id":28,"name":"Vehicles"},{"id":29,"name":"Entertainment: Comics"},{"id":30,"name":"Science: Gadgets"},{"id":31,"name":"Entertainment: Japanese Anime & Manga"},{"id":32,"name":"Entertainment: Cartoon & Animations"}]}
-
+let question_correct = [];
+let question_correctC= [];
 const categories = document.getElementById("selectC");
 
 
@@ -14,6 +15,7 @@ function valueI () {
 const selectD = document.getElementById("selectD").value;
 const selectT = document.getElementById("selectT").value;
 const selectC = document.getElementById("selectC").value;
+const form2 = document.getElementById("form2");
 const number = document.getElementById("number").value;
 if (selectD!=="Selecciona la dificultad" && selectT!=="Selecciona el tipo" && selectC!=="Selecciona la categoria") {
 
@@ -21,12 +23,25 @@ if (selectD!=="Selecciona la dificultad" && selectT!=="Selecciona el tipo" && se
 .then((response)=>response.json())
 .then((data)=>addDatas(data))
 } else {
-    alert("no se pudo")
+    form2.innerHTML="";
+    form2.innerHTML=`<div class="col-md-6" style="margin: auto;">
+    <div class="alert alert-info">
+        <div class="alert-body">
+        Debes rellenar los campos necesarios para generar tu QUIZ.
+        </div>
+    </div>
+</div>`;
 }
 
 }
 
 function addDatas (data) {
+    for (let i=0; i<data.results.length; i++) {
+        question_correct.push(data.results[i].correct_answer);
+    }
+    question_correctC=[];
+    question_correctC.push(...question_correct);
+    question_correct=[];
 const button = document.createElement("button");
 const selectT = document.getElementById("selectT").value;
 button.textContent="Enviar respuestas";
@@ -55,80 +70,106 @@ if (data.response_code==0) {
         for (let j=0; j<4; j++) {
 
             container2.lastElementChild.innerHTML+=`<div class="form-check col-md-6" style="margin: auto;">
-            <input value="${copy[j]}" class="form-check-input" type="radio" name="${v}" id="${mult}" required>
-            <label class="form-check-label" for="${mult}">
-            ${copy[j]}
-            </label>
-            </div>`;
+                                                        <input value="${copy[j]}" class="form-check-input" type="radio" name="${v}" id="${mult}" required>
+                                                            <label class="form-check-label" for="${mult}">
+                                                                ${copy[j]}
+                                                            </label>
+                                                    </div>`;
             mult++;
-        }
+            }
         v++;
     }
-    container2.lastElementChild.innerHTML+=`<button id="buttonM" class="btn btn-primary button-add">Enviar respuestas</button>`;
-
+    container2.lastElementChild.innerHTML+=`<button id="buttonC" class="btn btn-primary button-add">Enviar respuestas</button>`;
 } else {
     let booleano=["False","True"];
     for (let i=0; i<data.results.length; i++) {
         container2.lastElementChild.innerHTML+=`<div class="col-md-6" style="margin: auto; margin-top: 30px;">
-        <div class="card">
-            <div class="card-body">
-                ${data.results[i].question}
-            </div>
-        </div>`;
+                                                        <div class="card">
+                                                            <div class="card-body">
+                                                            ${data.results[i].question}
+                                                        </div>
+                                                </div>`;
         for (let j=0; j<2; j++) {
             container2.lastElementChild.innerHTML+=`<div class="form-check col-md-6" style="margin: auto;">
-            <input value="${booleano[j]}" class="form-check-input" type="radio" name="${inC}" id="${inCC}" required>
-            <label class="form-check-label" for="${inCC}">
-            ${booleano[j]}
-            </label>
-            </div>`;
+                                                             <input value="${booleano[j]}" class="form-check-input" type="radio" name="${inC}" id="${inCC}" required>
+                                                                <label class="form-check-label" for="${inCC}">
+                                                                        ${booleano[j]}
+                                                                </label>
+                                                    </div>`;
             inCC++;
         }
         inC++;
     }
-    container2.lastElementChild.innerHTML+=`<button id="buttonC" class="btn btn-primary button-add form-check">Enviar respuestas</button>`;
-
-}
-    
-    } else {
-        container2.innerHTML+=`<div class="col-md-6" style="margin: auto;">
-                                                            <div class="card">
-                                                                <div class="card-body">
+        container2.lastElementChild.innerHTML+=`<button id="buttonC" class="btn btn-primary button-add form-check">Enviar respuestas</button>`;
+        }
+    } else {    
+        container2.lastElementChild.innerHTML+=`<div class="col-md-6" style="margin: auto;">
+                                                            <div class="alert alert-info">
+                                                                <div class="alert-body">
                                                                 Sin resultados, no se pudieron devolver los resultados. La API no tiene suficientes preguntas para su consulta. (Por ejemplo, pedir 50 preguntas en una categoría que solo tiene 20)
                                                                 </div>
                                                             </div>
                                                  </div>`;
-}
-} 
-
+        }
+    } 
 
 function buttonD () {
-const selectD = document.getElementById("selectD").value;
-const selectT = document.getElementById("selectT").value;
-const selectC = document.getElementById("selectC").value;
-const number = document.getElementById("number").value;
-
-fetch(`https://opentdb.com/api.php?amount=${number}&category=${selectC}&difficulty=${selectD}&type=${selectT}`)
-.then((response)=>response.json())
-.then((data)=> buttonCS(data))
+    buttonCS();
 }
 
-function buttonCS (data) {
-    debugger;
+function buttonCS () {
+    const selec = document.getElementById("selectT").value;
     const form2 = document.getElementById("form2");
+    let count = 0;
     let cont=0;
-    for (let i = 0; i<data.results.length; i++) {
-        if (document.getElementById(`${i}`).value==data.results[i].correct_answer) {
-            cont++;
-            console.log ("Son iguales")
+    /* const form2 = document.getElementById("form2");
+    const selec = document.getElementById("selectT").value;
+    let count = 0;
+    let cont=0;
+        if (selec=="boolean") {
+            for (let i = 0; i<question_correctC.length; i++) {
+                for (let j=0; j<2; j++) {
+                    if (document.getElementById(`${count}`).value===question_correctC[i]) {
+                                cont++;
+                            }
+                        count++;
+                    }
+                }
         }
-    }
     form2.innerHTML="";
     form2.innerHTML=`<div class="col-md-6" style="margin: auto;">
-    <div class="card">
-        <div class="card-body">
-        Tienes ${cont} respuestas correctas de un total de ${data.results.length} preguntas. 
+    <div class="alert alert-info">
+        <div class="alert-body">
+        Tienes ${cont} respuestas correctas de un total de ${question_correctC.length} preguntas.
+        </div>
+    </div>
+</div>`; */
+if (selec=="boolean") {
+    for (let i = 0; i<question_correctC.length; i++) {
+        for (let j=0; j<2; j++) {
+            if (document.getElementById(`${count}`).value===question_correctC[i]) {
+                        cont++;
+                    }
+                count++;
+            }
+        }
+} else {
+    for (let i = 0; i<question_correctC.length; i++) {
+        for (let j=0; j<4; j++) {
+            if (document.getElementById(`${count}`).value===question_correctC[i]) {
+                        cont++;
+                    }
+                count++;
+            }
+        }
+}
+form2.innerHTML="";
+    form2.innerHTML=`<div class="col-md-6" style="margin: auto;">
+    <div class="alert alert-info">
+        <div class="alert-body">
+        Tienes ${cont} respuestas correctas de un total de ${question_correctC.length} preguntas.
         </div>
     </div>
 </div>`;
+question_correctC = [];
 }
